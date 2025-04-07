8440c7a566145e9f2be3190a71be4f1d155963c8
Siypr <68062506+SiyprYT@users.noreply.github.com>
2025-04-07 08:58:42 +0800

    Update encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -1,7 +1,7 @@
 #Tristan Le - 3/04/2025
 
-def encryption(plaintext, keyword):
-    ciphertext = []  # store characters
+def encryption(plainText, keyword):
+    cipherText = []  # store characters
     keyword_index = 0  # track position of the thingy
     
     print("")
@@ -17,21 +17,21 @@ def encryption(plaintext, keyword):
     print(" ")
     #totally didnt steal this from the code i made 2 years ago for computer science.... would never do that...
 
-    for char in plaintext:
+    for char in plainText:
         key_char = keyword[keyword_index % len(keyword)]  # if user makes the keyword 3 letters but the message is more, it loops it till it is the same or sum sum
         
         if char.isalpha():  # is the character a letter?!?!?!?!?!?
             shift = ord(keyword[keyword_index % len(keyword)]) - ord('a')  # Calculate the shift based on the keyword
             encrypted_char = chr(((ord(char) - ord('a') + shift) % 26) + ord('a'))  # adds the encryption thing on character
-            ciphertext.append(encrypted_char)  # add character to list
+            cipherText.append(encrypted_char)  # add character to list
             keyword_index += 1  # NEXT CHARACTER!
         elif char.isdigit():  # is the character a number?!?!?!?!?!?
             shift = ord(keyword[keyword_index % len(keyword)]) - ord('a')  # Use the keyword for digit shift as well
             encrypted_char = chr(((ord(char) - ord('0') + shift) % 10) + ord('0'))  # puts the encryption thingy on number
-            ciphertext.append(encrypted_char)  # add digit to the list
+            cipherText.append(encrypted_char)  # add digit to the list
             keyword_index += 1  # Move to the next character in the keyword
         else:
             # if it isnt a letter or number, dont change
-            ciphertext.append(char)
+            cipherText.append(char)
     
-    return ''.join(ciphertext)  # turns the list into string. wow!
+    return ''.join(cipherText)  # turns the list into string. wow!
